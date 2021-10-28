332ba851fced9f9305f83cf522a6b85c3a5220b0
Jeremy <jeremyeparker@yahoo.com>
2021-10-28 19:25:10 -0400

    test

---
+++ src/test/java/com/atsignJar/EmployeeSys/EmployeeSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsignJar.EmployeeSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
