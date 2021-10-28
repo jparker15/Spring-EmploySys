4bb7f6090d1e592ec06ba0eb59cc1438963cd91d
Jeremy <jeremyeparker@yahoo.com>
2021-10-28 19:24:25 -0400

    app

---
+++ src/main/java/com/atsignJar/EmployeeSys/EmployeeSysApplication.java
@@ -0,0 +1,14 @@
+package com.atsignJar.EmployeeSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSysApplication.class, args);
+	}
+
+
+}
